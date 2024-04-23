a5f00da53ec31896c18898693036895e32cc485a
arseth229 <146048786+arseth229@users.noreply.github.com>
2024-04-23 11:04:49 -0500

    finished

---
+++ index.js
@@ -0,0 +1,14 @@
+const {sayHelloTo} = require('./send-messages/say-hello-to')
+const giveMessageToMrsPotato = require('./send-messages/give-message-to-mrs-potato')
+// Your code here 
+const {msg1, msg2, msg3} = require('./messages/index')
+
+
+
+/****************************************************************************/
+/******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
+
+sayHelloTo("Mr. Potato");
+giveMessageToMrsPotato(msg1);
+giveMessageToMrsPotato(msg2);
+giveMessageToMrsPotato(msg3);
